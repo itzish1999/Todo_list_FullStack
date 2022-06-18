bae60dc3eee9aad0be13ec97e6c7f337ce06e805
itzish1999 <ismaeel.butt19@gmail.com>
2022-06-17 20:19:52 -0400

    feat(DB Setup)

---
+++ backend/config/db.config.js
@@ -0,0 +1,22 @@
+const { Pool } = require("pg");
+
+// Connect database to app
+module.exports = {
+    SERVER: 'localhost',
+    USER: 'postgres',
+    DB: 'employees',
+    PASSWORD: 'Alpha123Beta456Charlie789',
+    dialect: 'postgres',
+    Pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+/* First 5 params are mandatory to connect the database to the app.
+You have to and MUST put in the right information pertaining to the user, password, and database.
+If I am working on a already deployed db I would just adjust the remaining code */
+
+// Everything in pool is optional and are ment for the responses and limits I set for the app.
