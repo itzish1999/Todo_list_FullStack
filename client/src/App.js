32bb91ebc072c1247f0027bfb50521e8c9f0b1c0
itzish1999 <ismaeel.butt19@gmail.com>
2022-06-27 13:11:57 -0400

    feat(Client Setup)

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit <code>Front-End Setup</code>
+        </p>
+    </div>
+  );
+}
+
+export default App;
